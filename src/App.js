f8f666ff48938933e30783e9956c34f04374bf00
Chris Hastings-Spital <chrismhs@gmail.com>
2019-01-13 16:34:35 +0000

    Switched into a component framework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Biography from './Components/Bio'
 
 class App extends Component {
   render() {
@@ -9,21 +10,12 @@ class App extends Component {
         <header className="App-header">
           <div className="nav">
             <div className="container">
-              <div className="row">
-                <div class="col-md-6 col-md-offset-4" id="logo">chris.</div>
-                <div className="col-md-2"> <p>Contact</p> </div>
+              <div id="logo">chris.
+                <div id="contact"> <p>Contact</p> </div>
               </div>
             </div>
           </div>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-10 col-lg-8">
-                <h1>Chris, a product designer based in London.</h1>
-                <h2>Currently working at Triptease, helping hotels drive direct bookings.</h2>
-              </div>
-            </div>
-
-          </div>
+          <Biography />
         </header>
       </div>
     );
