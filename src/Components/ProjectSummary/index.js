63757f4b228019c7cdb6b14777d818eac9e40ba2
Chris Hastings-Spital <Chris@macbook.lan>
2019-04-02 08:10:57 +0100

    Animation of projects page
    Specifically for Triptease page but hasnt been separated out as a new component yet. It currently shows in every project summary.

--- src/Components/ProjectSummary/index.js
+++ src/Components/ProjectSummary/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import styled, { withTheme } from "styled-components";
 
+import Particles from 'react-particles-js';
+
 const MainImg = styled.img`
 position: absolute;
 margin-top: -80px;
+`
+
+const Background = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 800px;
 `
 
 const Container = styled.div`
@@ -22,7 +32,71 @@ const SectionHeader = styled.h2`
 class ProjectSummary extends Component {
     render() {
         return (
+            
             <Container className="container">
+            <Background>
+                <Particles
+                    params={{
+                        particles: {
+                            number: {
+                                value: "50",
+                                density: {
+                                  enable: true,
+                                  value_area: "800"
+                                }
+                              },
+                              color: {
+                                value: "#CCCCCC"
+                              },
+                              shape: {
+                                type: "circle",
+                                stroke: {
+                                  width: "0",
+                                  color: "#000000"
+                                },
+                                polygon: {
+                                  nb_sides: "5"
+                                },
+                                image: {
+                                  src: "img/github.svg",
+                                  width: "100",
+                                  height: "100"
+                                }
+                              },
+                              opacity: {
+                                value: "1",
+                                random: false,
+                                anim: {
+                                  enable: true,
+                                  speed: "0.001",
+                                  opacity_min: "0.1",
+                                  sync: true
+                                }
+                              },
+                              size: {
+                                value: "2",
+                                random: false,
+                                anim: {
+                                  enable: false,
+                                  speed: "20",
+                                  size_min: "0.1",
+                                  sync: false
+                                }
+                              },
+                            line_linked: {
+                                enable: true,
+                                distance: "150",
+                                color: "#CCCCCC",
+                                opacity: "1",
+                                width: "1"
+                            }
+                        }
+                    }}
+                  style={{
+                    width: '100%',
+                  }}
+                />
+            </Background>
                 <div className="row">
                     <div className="col-md-6 col-sm-10 offset-sm-1 offset-md-0">
                         <ProjectLogo className="col-6 ">
