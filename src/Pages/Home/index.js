132492d62f9863d33cdc1a2d610b65207c79fa18
Chris Hastings-Spital <chrismhs@gmail.com>
2019-04-21 09:50:33 +0100

    Removed similar animations

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled, { withTheme, keyframes } from "styled-components";
+import styled, { withTheme } from "styled-components";
 
 import Biography from '../../Components/Bio'
 import CaseStudies from '../../Components/CaseStudies'
@@ -7,33 +7,19 @@ import NewsFeed from '../../Components/NewsFeed'
 
 import theme from '../../styles/theme.style';
 
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-
-  to {
-    opacity: 1;
-  }
-`;
-
-const Container = styled.div`
-    animation: ${fadeIn} 2s cubic-bezier(0.165, 0.84, 0.44, 1);
-`
-
 class Home extends Component {
   changeColor(){
     this.props.changeThemeColor(theme.primary)
   }
   render() {
       return (
-          <Container className="page" onLoad={this.changeColor.bind(this)}>
+          <div className="page" onLoad={this.changeColor.bind(this)}>
               <Biography />
               <CaseStudies
                   changeThemeColor={this.props.changeThemeColor}
               />
               <NewsFeed />
-          </Container>
+          </div>
       )
   }
 }
