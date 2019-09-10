4a1a6d46a9d0fe05519be753585c1c76e465d684
Chris Hastings-Spital <chrismhs@gmail.com>
2019-09-10 20:43:51 +0100

    Experimenting with tracking

--- src/Components/Container/index.js
+++ src/Components/Container/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Route, withRouter, Switch } from "react-router-dom";
+import createHistory from 'history/createBrowserHistory'
+import ReactGA from 'react-ga';
 
 import Home from '../../Pages/Home'
 import Contact from '../../Pages/Contact'
 import Triptease from '../../Pages/Projects/Triptease'
 import Fixr from '../../Pages/Projects/Fixr'
 
+const history = createHistory()
+ReactGA.initialize('UA-131855428-2');
+history.listen((location, action) => {
+    ReactGA.pageview(location.pathname + location.search);
+    console.log(location.pathname)
+});
+
 function Container({ changeThemeColor }) {
     return (
         <div className="container">
-            <Route render={({ location }) => (
+            <Route history={history} render={({ location }) => (
                 <Switch>
                     <Route exact path="/" render={() => <Home changeThemeColor={changeThemeColor} />} />
                     <Route path="/contact" component={Contact} />
