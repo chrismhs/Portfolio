7ca7c8f1a6131e6eea79a13bdb6edae713c6657d
Chris Hastings-Spital <chrismhs@gmail.com>
2020-03-03 15:46:03 +0000

    Removing homepage work

--- src/Components/CaseStudies/CaseStudyLeft/index.js
+++ src/Components/CaseStudies/CaseStudyLeft/index.js
@@ -1,98 +1,91 @@
 import React, { Component } from "react";
 import { BrowserRouter as Route, Router, Link } from "react-router-dom";
-import styled, { withTheme } from "styled-components"
+import styled, { withTheme } from "styled-components";
 
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 
 const CaseStudyContainer = styled.div`
-    background-color: rgb(${props => props.projecttheme.background});
-    color: rgb(${props => props.projecttheme.foreground});
-    height: 550px;
-    border-radius: 20px;
-    margin-bottom: 50px;
+  background-color: rgb(${props => props.projecttheme.background});
+  color: rgb(${props => props.projecttheme.foreground});
+  height: 550px;
+  border-radius: 20px;
+  margin-bottom: 50px;
 
-    @media (max-width: 992px) {
-        height: 450px;
-      }
-`
+  @media (max-width: 992px) {
+    height: 450px;
+  }
+`;
 
 const ProjectLogo = styled.div`
-    height: auto;
-    padding: 100px 0 40px 0;
-    width: ${props => props.logoWidth};
+  height: auto;
+  padding: 100px 0 40px 0;
+  width: ${props => props.logoWidth};
 
-    @media (max-width: 992px) {
-        padding: 40px 0 40px 0;
-      }
-`
+  @media (max-width: 992px) {
+    padding: 40px 0 40px 0;
+  }
+`;
 
-const ProjectLink =  styled(Link)`
+const ProjectLink = styled(Link)`
+  color: rgb(${props => props.projecttheme.link});
+  padding-bottom: 4px;
+  border-bottom: 2px solid rgb(${props => props.projecttheme.link});
+
+  :hover {
     color: rgb(${props => props.projecttheme.link});
-    padding-bottom: 4px;
-    border-bottom: 2px solid rgb(${props => props.projecttheme.link});
+    filter: brightness(120%);
+  }
 
-    :hover {
-        color: rgb(${props => props.projecttheme.link});
-        filter: brightness(120%);
-    }
-
-    :after {
-        color: rgb(${props => props.projecttheme.background});
-        background: rgb(${props => props.projecttheme.link});
-    }
-`
+  :after {
+    color: rgb(${props => props.projecttheme.background});
+    background: rgb(${props => props.projecttheme.link});
+  }
+`;
 
 const MainImg = styled.img`
-    height: 550px;
-    padding: 30px;
-
-    ${props => props.showAnim ?
-        `display: none;` :
-        null
-    };
-
-`
-
-const AnimationContainer = styled.div`
-height: 550px;
-background-color: red;
-`
+  height: 550px;
+  padding: 30px;
+`;
 
 class CaseStudyLeft extends Component {
-    render() {
-        return (
-            <div>
-                <CaseStudyContainer projecttheme={this.props.projecttheme}>
-                    <div className="row">
-                        <div className="col-10 col-lg-5 offset-1">
-                            <ProjectLogo logoWidth={this.props.logoWidth}>
-                                <img
-                                    src={this.props.logo}
-                                    alt={this.props.logoalt}
-                                    className="img-fluid"
-                                />
-                            </ProjectLogo>
-                            <h2>{this.props.title}</h2>
-                            <p>{this.props.body}</p>
-                            <ProjectLink projecttheme={this.props.projecttheme} to={this.props.projectLink} href="">View case study</ProjectLink>
-                        </div>
-                        <div className="text-center col-6 d-none d-lg-block overflow-hidden">
-                            <Fade>
-                                <MainImg
-                                    showAnim={this.props.showAnim}
-                                    src={this.props.mainImage}
-                                    alt={this.props.imagealt}
-                                    className="rounded"
-                                >
-                                </MainImg>
-                                <AnimationContainer>{this.props.imgAnim}</AnimationContainer>
-                            </Fade>
-                        </div>
-                    </div>
-                </CaseStudyContainer>
+  render() {
+    return (
+      <div>
+        <CaseStudyContainer projecttheme={this.props.projecttheme}>
+          <div className="row">
+            <div className="col-10 col-lg-5 offset-1">
+              <ProjectLogo logoWidth={this.props.logoWidth}>
+                <img
+                  src={this.props.logo}
+                  alt={this.props.logoalt}
+                  className="img-fluid"
+                />
+              </ProjectLogo>
+              <h2>{this.props.title}</h2>
+              <p>{this.props.body}</p>
+              <ProjectLink
+                projecttheme={this.props.projecttheme}
+                to={this.props.projectLink}
+                href=""
+              >
+                View case study
+              </ProjectLink>
             </div>
-        )
-    }
+            <div className="text-center col-6 d-none d-lg-block overflow-hidden">
+              <Fade>
+                <MainImg
+                  showAnim={this.props.showAnim}
+                  src={this.props.mainImage}
+                  alt={this.props.imagealt}
+                  className="rounded"
+                ></MainImg>
+              </Fade>
+            </div>
+          </div>
+        </CaseStudyContainer>
+      </div>
+    );
+  }
 }
 
 export default withTheme(CaseStudyLeft);
