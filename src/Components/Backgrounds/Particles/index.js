cf70527fa752d35c79769b29b3f335b7c92b275e
Chris Hastings-Spital <chrismhs@gmail.com>
2020-03-24 20:17:50 +0000

    Hack around build issues

--- src/Components/Backgrounds/Particles/index.js
+++ src/Components/Backgrounds/Particles/index.js
@@ -1,85 +1,88 @@
-import React from 'react';
-import Particles from 'react-particles-js';
+import React from "react";
+/*
+I don't know why this isn't working at the moment
+
+
+import Particles from "react-particles-js";
 
 import styled from "styled-components";
 
 const ParticleContainer = styled(Particles)`
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 50%;
-    height: 100vh
+	position: absolute;
+	right: 0;
+	top: 0;
+	width: 50%;
+	height: 100vh @media (max-width: 768px) {
+		width: 100%;
+		height: 50vh;
+	}
+`;
 
-    @media (max-width: 768px) {
-        width: 100%;
-        height: 50vh
-    }
-`
-
-export default class ParticleBackground extends React.Component {
-    render(){
-        return (
-            <ParticleContainer
-                params={{
-                    particles: {
-                        number: {
-                            value: "50",
-                            density: {
-                                enable: true,
-                                value_area: "800"
-                            }
-                            },
-                            color: {
-                            value: "#CCCCCC"
-                            },
-                            shape: {
-                            type: "circle",
-                            stroke: {
-                                width: "0",
-                                color: "#000000"
-                            },
-                            polygon: {
-                                nb_sides: "5"
-                            },
-                            image: {
-                                src: "img/github.svg",
-                                width: "100",
-                                height: "100"
-                            }
-                            },
-                            opacity: {
-                            value: "1",
-                            random: false,
-                            anim: {
-                                enable: true,
-                                speed: "0.001",
-                                opacity_min: "0.1",
-                                sync: true
-                            }
-                            },
-                            size: {
-                            value: "2",
-                            random: false,
-                            anim: {
-                                enable: false,
-                                speed: "20",
-                                size_min: "0.1",
-                                sync: false
-                            }
-                            },
-                        line_linked: {
-                            enable: true,
-                            distance: "150",
-                            color: "#CCCCCC",
-                            opacity: "1",
-                            width: "1"
-                        }
-                    }
-                }}
-                style={{
-                width: '100%',
-                }}
-            />
-        );
-    }
-}
+	export default class ParticleBackground extends React.Component {
+		render() {
+			return (
+				<ParticleContainer
+					params={{
+						particles: {
+							number: {
+								value: "50",
+								density: {
+									enable: true,
+									value_area: "800"
+								}
+							},
+							color: {
+								value: "#CCCCCC"
+							},
+							shape: {
+								type: "circle",
+								stroke: {
+									width: "0",
+									color: "#000000"
+								},
+								polygon: {
+									nb_sides: "5"
+								},
+								image: {
+									src: "img/github.svg",
+									width: "100",
+									height: "100"
+								}
+							},
+							opacity: {
+								value: "1",
+								random: false,
+								anim: {
+									enable: true,
+									speed: "0.001",
+									opacity_min: "0.1",
+									sync: true
+								}
+							},
+							size: {
+								value: "2",
+								random: false,
+								anim: {
+									enable: false,
+									speed: "20",
+									size_min: "0.1",
+									sync: false
+								}
+							},
+							line_linked: {
+								enable: true,
+								distance: "150",
+								color: "#CCCCCC",
+								opacity: "1",
+								width: "1"
+							}
+						}
+					}}
+					style={{
+						width: "100%"
+					}}
+				/>
+			);
+		}
+	}
+*/
